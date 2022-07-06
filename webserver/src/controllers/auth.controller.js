ed1716dc58de30308d204fd4c7489925fef5bf97
Shoeb Joarder <shoebjoarder@outlook.com>
2022-07-06 17:03:15 +0200

    Update auth.controller.js

--- webserver/src/controllers/auth.controller.js
+++ webserver/src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
-import {sign} from "jsonwebtoken";
-import {compareSync, hashSync} from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { compareSync, hashSync } from "bcryptjs";
 
 const config = require("../config/auth.config");
 const db = require("../models");
@@ -17,8 +17,8 @@ const Role = db.role;
  * @param {string} req.body.password The new password
  * @param {Array} req.body.role The roles e.g., ['user']
  */
-export const signup = (req, res) => {
-  const user = new User({
+export const signup = async (req, res) => {
+  let user = new User({
     firstname: req.body.firstname,
     lastname: req.body.lastname,
     username: req.body.username,
@@ -26,55 +26,43 @@ export const signup = (req, res) => {
     password: hashSync(req.body.password, 8),
   });
 
-  user.save((err, user) => {
-    if (err) {
-      res.status(500).send({error: err});
-      return;
+  try {
+    await user.save();
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+
+  if (req.body.roles) {
+    try {
+      let roles = await Role.find({ name: { $in: req.body.roles } });
+      user.roles = roles.map((role) => role._id);
+    } catch (err) {
+      return res.status(500).send({ error: err });
     }
 
-    if (req.body.roles) {
-      Role.find(
-        {
-          name: {$in: req.body.roles},
-        },
-        (err, roles) => {
-          if (err) {
-            res.status(500).send({error: err});
-            return;
-          }
-
-          user.roles = roles.map((role) => role._id);
-          user.save((err) => {
-            if (err) {
-              res.status(500).send({error: err});
-              return;
-            }
-
-            res
-              .status(200)
-              .send({success: "User is successfully registered!"});
-          });
-        }
-      );
-    } else {
-      Role.findOne({name: "user"}, (err, role) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-
-        user.roles = [role._id];
-        user.save((err) => {
-          if (err) {
-            res.status(500).send({error: err});
-            return;
-          }
-
-          res.status(200).send({success: "User is successfully registered!"});
-        });
-      });
+    try {
+      await user.save();
+      res.status(200).send({ success: "User is successfully registered!" });
+    } catch (err) {
+      return res.status(500).send({ error: err });
     }
-  });
+  } else {
+    try {
+      let role = await Role.findOne({ name: "user" });
+      user.roles = [role._id];
+    } catch (err) {
+      return res.status(500).send({ error: err });
+    }
+
+    try {
+      await user.save();
+      return res
+        .status(200)
+        .send({ success: "User is successfully registered!" });
+    } catch (err) {
+      return res.status(500).send({ error: err });
+    }
+  }
 };
 
 /**
@@ -84,47 +72,49 @@ export const signup = (req, res) => {
  * @param {string} req.body.username The username
  * @param {string} req.body.password The new password
  */
-export const signin = (req, res) => {
-  User.findOne({username: req.body.username})
-    .populate("roles", "-__v")
-    .exec((err, user) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
+export const signin = async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
-      if (!user) {
-        return res.status(404).send({error: "User not found."});
-      }
+  try {
+    let user = await User.findOne({ username: username }).populate(
+      "roles",
+      "-__v"
+    );
+    if (!user) {
+      return res.status(404).send({ error: "User not found." });
+    }
 
-      let passwordIsValid = compareSync(req.body.password, user.password);
+    let passwordIsValid = await compareSync(password, user.password);
 
-      if (!passwordIsValid) {
-        return res.status(401).send({error: "Invalid Password!"});
-      }
+    if (!passwordIsValid) {
+      return res.status(401).send({ error: "Invalid Password!" });
+    }
 
-      let token = sign({id: user.id}, config.secret, {
-        expiresIn: 86400, // 24 hours
-      });
-
-      let authorities = [];
-
-      for (let i = 0; i < user.roles.length; i++) {
-        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-      }
-
-      req.session.token = token;
-
-      const userName = `${user.firstname} ${user.lastname}`;
-
-      res.status(200).send({
-        id: user._id,
-        name: userName,
-        username: user.username,
-        email: user.email,
-        roles: authorities,
-      });
+    let token = sign({ id: user.id }, config.secret, {
+      expiresIn: 86400, // 24 hours
     });
+
+    let authorities = [];
+
+    for (let i = 0; i < user.roles.length; i++) {
+      authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+    }
+
+    req.session.token = token;
+
+    const userName = `${user.firstname} ${user.lastname}`;
+
+    return res.status(200).send({
+      id: user._id,
+      name: userName,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+    });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 };
 
 /**
@@ -135,7 +125,7 @@ export const signin = (req, res) => {
 export const signout = async (req, res) => {
   try {
     req.session = null;
-    return res.status(200).send({success: "You've been signed out!"});
+    return res.status(200).send({ success: "You've been signed out!" });
   } catch (err) {
     this.next(err);
   }
