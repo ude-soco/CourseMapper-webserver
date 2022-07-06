bc032c6d535fe05739a9f48d7b433659f444faef
Shoeb Joarder <shoebjoarder@outlook.com>
2022-07-06 19:18:09 +0200

    Update channel.controller.js

--- webserver/src/controllers/channel.controller.js
+++ webserver/src/controllers/channel.controller.js
@@ -11,170 +11,136 @@ const Topic = db.topic;
  *
  * @param {string} req.params.channelId The id of the channel
  */
-export const getChannel = (req, res) => {
+export const getChannel = async (req, res) => {
   const channelId = req.params.channelId;
-  Channel.findOne({_id: ObjectId(channelId)})
-    .populate("materials", "-__v")
-    .exec((err, foundChannel) => {
-      if (err) {
-        res.status(500).send({message: err});
-        return;
-      }
-      res.status(200).send(foundChannel);
-    });
+  try {
+    let foundChannel = await Channel.findOne({
+      _id: ObjectId(channelId),
+    }).populate("materials", "-__v");
+    return res.status(200).send(foundChannel);
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
 };
 
 /**
  * @function newChannel
- * Get a course detail in database controller
+ * Create a new channel controller
  *
  * @param {string} req.params.topicId The id of the topic
  * @param {string} req.body.name The name of the channel, e.g., React Crash Course
  * @param {string} req.body.description The description of the channel, e.g., Teaching about React
  * @param {string} req.userId The owner of the channel
  */
-export const newChannel = (req, res) => {
-  Topic.findOne({_id: ObjectId(req.params.topicId)}, (err, foundTopic) => {
-    if (err) {
-      res.status(500).send({error: err});
-      return;
-    }
+export const newChannel = async (req, res) => {
+  const topicId = req.params.topicId;
+  const channelName = req.body.name;
+  const channelDesc = req.body.description;
 
+  let foundTopic;
+  try {
+    foundTopic = await Topic.findOne({ _id: ObjectId(topicId) });
     if (!foundTopic) {
-      res.status(404).send({
-        error: `Topic with id ${req.params.topicId} doesn't exist!`,
+      return res.status(404).send({
+        error: `Topic with id ${topicId} doesn't exist!`,
       });
-      return;
     }
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 
-    const channel = new Channel({
-      name: req.body.name,
-      description: req.body.description,
-      courseId: foundTopic.courseId,
-      topicId: req.params.topicId,
-      userId: req.userId,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    });
-
-    channel.save((err, channel) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
-
-      res.send({
-        id: channel._id,
-        success: `New channel '${channel.name}' added!`,
-      });
-
-      let channels = [];
-      channels.push(channel._id);
-
-      foundTopic.channels.push(channel._id);
-
-      foundTopic.save((err, topic) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-
-        Course.findOne({_id: topic.courseId}, (err, updateCourse) => {
-          if (err) {
-            res.status(500).send({error: err});
-            return;
-          }
-
-          updateCourse.channels.push(channel._id);
-
-          updateCourse.save((err) => {
-            if (err) {
-              res.status(500).send({error: err});
-              return;
-            }
-          });
-        });
-      });
-    });
+  let channel = new Channel({
+    name: channelName,
+    description: channelDesc,
+    courseId: foundTopic.courseId,
+    topicId: topicId,
+    userId: req.userId,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
   });
+
+  let savedChannel;
+  try {
+    savedChannel = await channel.save();
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  foundTopic.channels.push(savedChannel._id);
+
+  let savedTopic;
+  try {
+    savedTopic = await foundTopic.save();
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  let updateCourse;
+  try {
+    updateCourse = await Course.findOne({ _id: savedTopic.courseId });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  updateCourse.channels.push(savedChannel._id);
+
+  try {
+    await updateCourse.save();
+
+    res.send({
+      id: channel._id,
+      success: `New channel '${channel.name}' added!`,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 };
 
 /**
  * @function deleteChannel
- * Delete a topic controller
+ * Delete a channel controller
  *
  * @param {string} req.params.channelId The id of the channel
  */
-export const deleteChannel = (req, res) => {
-  Channel.findByIdAndRemove(
-    {_id: req.params.channelId},
-    (err, foundChannel) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
+export const deleteChannel = async (req, res) => {
+  const channelId = req.params.channelId;
 
-      if (!foundChannel) {
-        res.status(404).send({
-          error: `Channel with id ${req.params.channelId} doesn't exist!`,
-        });
-        return;
-      }
-
-      Material.deleteMany({_id: {$in: foundChannel.materials}}, (err) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-      });
-
-      Topic.findOne({_id: foundChannel.topicId}, (err, foundTopic) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-
-        let topicIndex = foundTopic["channels"].indexOf(
-          ObjectId(req.params.channelId)
-        );
-
-        if (topicIndex >= 0) {
-          foundTopic["channels"].splice(topicIndex, 1);
-        }
-
-        foundTopic.save((err) => {
-          if (err) {
-            res.status(500).send({error: err});
-            return;
-          }
-        });
-      });
-
-      Course.findOne({_id: foundChannel.courseId}, (err, foundCourse) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-
-        let channelIndex = foundCourse["channels"].indexOf(
-          ObjectId(req.params.channelId)
-        );
-
-        if (channelIndex >= 0) {
-          foundCourse["channels"].splice(channelIndex, 1);
-        }
-
-        foundCourse.save((err) => {
-          if (err) {
-            res.status(500).send({error: err});
-            return;
-          }
-        });
-      });
-
-      res.send({
-        success: `Channel '${foundChannel.name}' successfully deleted!`,
+  let foundChannel;
+  try {
+    foundChannel = await Channel.findByIdAndRemove({ _id: channelId });
+    if (!foundChannel) {
+      return res.status(404).send({
+        error: `Channel with id ${channelId} doesn't exist!`,
       });
     }
-  );
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  try {
+    await Material.deleteMany({ _id: { $in: foundChannel.materials } });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  let foundTopic;
+  try {
+    foundTopic = await Topic.findOne({ _id: foundChannel.topicId });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
+
+  let topicIndex = foundTopic["channels"].indexOf(ObjectId(channelId));
+  if (topicIndex >= 0) {
+    foundTopic["channels"].splice(topicIndex, 1);
+  }
+
+  try {
+    await foundTopic.save();
+    return res.send({
+      success: `Channel '${foundChannel.name}' successfully deleted!`,
+    });
+  } catch (err) {
+    res.status(500).send({ error: err });
+  }
 };
