49defd830985ca29d3e698d1537a66d778cc10ad
Shoeb Joarder <shoebjoarder@outlook.com>
2022-07-06 18:22:32 +0200

    Update topic.controller.js

--- webserver/src/controllers/topic.controller.js
+++ webserver/src/controllers/topic.controller.js
@@ -10,17 +10,17 @@ const Topic = db.topic;
  *
  * @param {string} req.params.topicId The id of the topic
  */
-export const getTopic = (req, res) => {
+export const getTopic = async (req, res) => {
   const topicId = req.params.topicId;
-  Topic.findOne({_id: ObjectId(topicId)})
-    .populate("channels", "-__v")
-    .exec((err, foundTopic) => {
-      if (err) {
-        res.status(500).send({message: err});
-        return;
-      }
-      res.status(200).send(foundTopic);
-    });
+  try {
+    let foundTopic = await Topic.findOne({ _id: ObjectId(topicId) }).populate(
+      "channels",
+      "-__v"
+    );
+    return res.status(200).send(foundTopic);
+  } catch (err) {
+    return res.status(500).send({ message: err });
+  }
 };
 
 /**
@@ -29,52 +29,50 @@ export const getTopic = (req, res) => {
  *
  * @param {string} req.params.courseId The id of the course
  * @param {string} req.body.name The name of the topic, e.g., React Crash Course
- * @param {string} req.body.description The description of the course, e.g., Teaching students about modern web technologies
  * @param {string} req.userId The owner of the topic
  */
-export const newTopic = (req, res) => {
-  Course.findOne({_id: ObjectId(req.params.courseId)}, (err, foundCourse) => {
-    if (err) {
-      res.status(500).send({error: err});
-      return;
-    }
+export const newTopic = async (req, res) => {
+  let courseId = req.params.courseId;
+  let topicName = req.body.name;
 
+  let foundCourse;
+  try {
+    foundCourse = await Course.findOne({ _id: ObjectId(courseId) });
     if (!foundCourse) {
-      res.status(404).send({
-        error: `Course with id ${req.params.courseId} doesn't exist!`,
+      return res.status(404).send({
+        error: `Course with id ${courseId} doesn't exist!`,
       });
-      return;
     }
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 
-    const topic = new Topic({
-      name: req.body.name,
-      courseId: req.params.courseId,
-      userId: req.userId,
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
-    });
-
-    topic.save((err, topic) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
-
-      res.send({id: topic._id, success: `New topic '${topic.name}' added!`});
-
-      let topics = [];
-      topics.push(topic._id);
-
-      foundCourse.topics.push(topic._id);
-
-      foundCourse.save((err) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-      });
-    });
+  let topic = new Topic({
+    name: topicName,
+    courseId: courseId,
+    userId: req.userId,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
   });
+
+  let savedTopic;
+  try {
+    savedTopic = await topic.save();
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+
+  foundCourse.topics.push(savedTopic._id);
+
+  try {
+    await foundCourse.save();
+    return res.send({
+      id: topic._id,
+      success: `New topic '${topicName}' added!`,
+    });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 };
 
 /**
@@ -83,46 +81,44 @@ export const newTopic = (req, res) => {
  *
  * @param {string} req.params.topicId The id of the topic
  */
-export const deleteTopic = (req, res) => {
-  Topic.findByIdAndRemove({_id: req.params.topicId}, (err, foundTopic) => {
-    if (err) {
-      res.status(500).send({error: err});
-      return;
-    }
+export const deleteTopic = async (req, res) => {
+  let topicId = req.params.topicId;
 
+  let foundTopic;
+  try {
+    foundTopic = await Topic.findByIdAndRemove({ _id: topicId });
     if (!foundTopic) {
-      res.status(404).send({
-        error: `Topic with id ${req.params.topicId} doesn't exist!`,
-      });
-      return;
+      return res
+        .status(404)
+        .send({ error: `Topic with id ${topicId} doesn't exist!` });
     }
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 
-    Channel.deleteMany({_id: {$in: foundTopic.channels}}, (err) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
+  try {
+    await Channel.deleteMany({ topicId: { $in: topicId } });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+
+  let foundCourse;
+  try {
+    foundCourse = await Course.findOne({ _id: foundTopic.courseId });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+
+  foundCourse.topics = foundCourse.topics.filter(
+    (topic) => topic.valueOf() !== topicId
+  );
+
+  try {
+    await foundCourse.save();
+    return res.send({
+      success: `Topic '${foundTopic.name}' successfully deleted!`,
     });
-
-    Course.findOne({_id: foundTopic.courseId}, (err, foundCourse) => {
-      if (err) {
-        res.status(500).send({error: err});
-        return;
-      }
-
-      let filteredTopics = foundCourse.topics.filter(
-        (topic) => topic.valueOf() !== req.params.topicId
-      );
-      foundCourse.topics = filteredTopics;
-
-      foundCourse.save((err) => {
-        if (err) {
-          res.status(500).send({error: err});
-          return;
-        }
-      });
-    });
-
-    res.send({success: `Topic '${foundTopic.name}' successfully deleted!`});
-  });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
 };
