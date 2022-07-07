25ab40482e50b5ac8f4611fa4a0513412f609e26
Shoeb Joarder <shoebjoarder@outlook.com>
2022-07-07 09:13:14 +0200

    refactored middlewares

--- webserver/src/middlewares/authJwt.js
+++ webserver/src/middlewares/authJwt.js
@@ -1,4 +1,4 @@
-import {verify} from "jsonwebtoken";
+import { verify } from "jsonwebtoken";
 
 const config = require("../config/auth.config.js");
 const db = require("../models");
@@ -9,78 +9,58 @@ const verifyToken = (req, res, next) => {
   let token = req.session.token;
 
   if (!token) {
-    return res.status(403).send({message: "No token provided!"});
+    return res.status(403).send({ message: "No token provided!" });
   }
 
   verify(token, config.secret, (err, decoded) => {
     if (err) {
-      return res.status(401).send({message: "Unauthorized!"});
+      return res.status(401).send({ message: "Unauthorized!" });
     }
     req.userId = decoded.id;
     next();
   });
 };
 
-const isAdmin = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({message: err});
-      return;
+const isAdmin = async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.userId);
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  let roles;
+  try {
+    roles = await Role.find({ _id: { $in: user.roles } });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "admin") {
+      return next();
     }
-
-    Role.find(
-      {
-        _id: {$in: user.roles},
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({message: err});
-          return;
-        }
-
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "admin") {
-            next();
-            return;
-          }
-        }
-
-        res.status(403).send({message: "Require Admin Role!"});
-        return;
-      }
-    );
-  });
+  }
+  return res.status(403).send({ message: "Require Admin Role!" });
 };
 
-const isModerator = (req, res, next) => {
-  User.findById(req.userId).exec((err, user) => {
-    if (err) {
-      res.status(500).send({message: err});
-      return;
+const isModerator = async (req, res, next) => {
+  let user;
+  try {
+    user = await User.findById(req.userId);
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  let roles;
+  try {
+    roles = await Role.find({ _id: { $in: user.roles } });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  for (let i = 0; i < roles.length; i++) {
+    if (roles[i].name === "moderator") {
+      return next();
     }
-
-    Role.find(
-      {
-        _id: {$in: user.roles},
-      },
-      (err, roles) => {
-        if (err) {
-          res.status(500).send({message: err});
-          return;
-        }
-
-        for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "moderator") {
-            next();
-            return;
-          }
-        }
-
-        res.status(403).send({message: "Require Moderator Role!"});
-        return;
-      }
-    );
-  });
+  }
+  return res.status(403).send({ message: "Require Moderator Role!" });
 };
 
 const authJwt = {
--- webserver/src/middlewares/verifySignUp.js
+++ webserver/src/middlewares/verifySignUp.js
@@ -2,53 +2,41 @@ const db = require("../models");
 const ROLES = db.ROLES;
 const User = db.user;
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  User.findOne({
-    username: req.body.username,
-  }).exec((err, user) => {
-    if (err) {
-      res.status(500).send({message: err});
-      return;
-    }
-
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+  let user;
+  try {
+    user = User.findOne({ username: req.body.username });
     if (user) {
-      res.status(400).send({message: "Failed! Username is already in use!"});
+      return res
+        .status(400)
+        .send({ message: "Failed! Username is already in use!" });
+    }
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  try {
+    user = User.findOne({ email: req.body.email });
+    if (user) {
+      res.status(400).send({ message: "Failed! Email is already in use!" });
       return;
     }
-
-    // Email
-    User.findOne({
-      email: req.body.email,
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({message: err});
-        return;
-      }
-
-      if (user) {
-        res.status(400).send({message: "Failed! Email is already in use!"});
-        return;
-      }
-
-      next();
-    });
-  });
+  } catch (err) {
+    return res.status(500).send({ error: err });
+  }
+  return next();
 };
 
 const checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
+        return res.status(400).send({
           message: `Failed! Role ${req.body.roles[i]} does not exist!`,
         });
-        return;
       }
     }
   }
-
-  next();
+  return next();
 };
 
 const verifySignUp = {
