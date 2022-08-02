337013b472658e379d5293aa4972626319930af6
Shoeb Joarder <shoebjoarder@outlook.com>
2022-08-03 00:12:38 +0200

    added pages

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,12 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ChannelbarComponent } from './components/channelbar/channelbar.component';
+import { SidebarComponent } from './pages/components/sidebar/sidebar.component';
+import { NavbarComponent } from './pages/components/navbar/navbar.component';
+import { ChannelbarComponent } from './pages/components/channelbar/channelbar.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, NavbarComponent, ChannelbarComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    ChannelbarComponent,
+    CoursesComponent,
+    ButtonComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, PrimengModule],
   providers: [],
   bootstrap: [AppComponent],
