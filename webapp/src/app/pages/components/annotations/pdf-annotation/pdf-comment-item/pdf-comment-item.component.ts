586405cad0d5cc1cda38ab699f54a678b6ba9fb5
x4ahmed <ahmed.mohamed.omran@stud.uni-due.de>
2023-02-14 16:00:24 +0100

    Moved show annotations on comment panel functionality to comment panel component

--- webapp/src/app/pages/components/annotations/pdf-annotation/pdf-comment-item/pdf-comment-item.component.ts
+++ webapp/src/app/pages/components/annotations/pdf-annotation/pdf-comment-item/pdf-comment-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { getInitials } from 'src/app/format';
 import { Annotation } from 'src/app/models/Annotations';
@@ -7,55 +7,12 @@ import { getAnnotationsForMaterial, State } from '../state/annotation.reducer';
 @Component({
   selector: 'app-pdf-comment-item',
   templateUrl: './pdf-comment-item.component.html',
-  styleUrls: ['./pdf-comment-item.component.css']
+  styleUrls: ['./pdf-comment-item.component.css'],
 })
 export class PdfCommentItemComponent implements OnInit {
-  annotationsWithInitials: Annotation[] = [];
-  userName: string;
-  initials: string;
+  @Input() annotationsWithInitials: Annotation[] = [];
 
-  constructor(private store: Store<State>) {
-    this.store.select(getAnnotationsForMaterial).subscribe((annotations) => {
-      this.setInitialsforEachAnnotation(annotations);
-    });
-   }
+  constructor(private store: Store<State>) {}
 
-  ngOnInit(): void {
-  }
-
-setInitialsforEachAnnotation(annotations: Annotation[]){
-  this.annotationsWithInitials = annotations.map((annotation) => ({
-    ...annotation, 
-    initials: getInitials(annotation.author.name),
-    elapsedTime: this.computeElapsedTime(annotation.createdAt)
-  }));
-}
-
-computeElapsedTime(timestamp: number | undefined): string {
-  if (!timestamp) return "";
-  let diffMs = new Date().getTime() - new Date(timestamp).getTime(); // milliseconds
-
-  let diffYears=Math.floor(diffMs/31536000000) // years
-  if(diffYears>0) return diffYears+" month(s) ago"
-
-  let diffMonths = Math.floor(diffMs / 2721600000); // month
-  if(diffMonths>0) return diffMonths+" month(s) ago"
-
-  let diffWeeks = Math.floor(diffMs / 604800000); // weeks
-  if(diffWeeks>0) return diffWeeks+" week(s) ago"
-
-  let diffDays = Math.floor(diffMs / 86400000); // days
-  if(diffDays>0) return diffDays+" day(s) ago"
-
-  let diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
-  if(diffHrs>0) return diffHrs+" hour(s) ago"
-
-  let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
-  if(diffMins>0) return diffMins+" minute(s) ago"
-
-  if(diffMs>0) return  "few seconds ago"
-
-  return "${timestamp}";
-}
-
+  ngOnInit(): void {}
 }
--- webapp/src/app/pages/components/annotations/pdf-annotation/pdf-comment-panel/pdf-comment-panel.component.ts
+++ webapp/src/app/pages/components/annotations/pdf-annotation/pdf-comment-panel/pdf-comment-panel.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Annotation } from 'src/app/models/Annotations';
+import { Store } from '@ngrx/store';
+import { getInitials } from 'src/app/format';
+import { getAnnotationsForMaterial, State } from '../state/annotation.reducer';
 @Component({
   selector: 'app-pdf-comment-panel',
   templateUrl: './pdf-comment-panel.component.html',
-  styleUrls: ['./pdf-comment-panel.component.css']
+  styleUrls: ['./pdf-comment-panel.component.css'],
 })
 export class PdfCommentPanelComponent implements OnInit {
+  annotationsWithInitials: Annotation[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store<State>) {
+    this.store.select(getAnnotationsForMaterial).subscribe((annotations) => {
+      this.setInitialsforEachAnnotation(annotations);
+    });
   }
 
+  ngOnInit(): void {}
+
+  setInitialsforEachAnnotation(annotations: Annotation[]) {
+    this.annotationsWithInitials = annotations.map((annotation) => ({
+      ...annotation,
+      initials: getInitials(annotation.author.name),
+      elapsedTime: this.computeElapsedTime(annotation.createdAt),
+    }));
+  }
+
+  computeElapsedTime(timestamp: number | undefined): string {
+    if (!timestamp) return '';
+    let diffMs = new Date().getTime() - new Date(timestamp).getTime(); // milliseconds
+
+    let diffYears = Math.floor(diffMs / 31536000000); // years
+    if (diffYears > 0) return diffYears + ' month(s) ago';
+
+    let diffMonths = Math.floor(diffMs / 2721600000); // month
+    if (diffMonths > 0) return diffMonths + ' month(s) ago';
+
+    let diffWeeks = Math.floor(diffMs / 604800000); // weeks
+    if (diffWeeks > 0) return diffWeeks + ' week(s) ago';
+
+    let diffDays = Math.floor(diffMs / 86400000); // days
+    if (diffDays > 0) return diffDays + ' day(s) ago';
+
+    let diffHrs = Math.floor((diffMs % 86400000) / 3600000); // hours
+    if (diffHrs > 0) return diffHrs + ' hour(s) ago';
+
+    let diffMins = Math.round(((diffMs % 86400000) % 3600000) / 60000); // minutes
+    if (diffMins > 0) return diffMins + ' minute(s) ago';
+
+    if (diffMs > 0) return 'few seconds ago';
+
+    return '${timestamp}';
+  }
 }
