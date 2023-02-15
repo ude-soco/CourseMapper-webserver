beaab3aac21e482902bbae73980d5ea769ece7c9
x4ahmed <ahmed.mohamed.omran@stud.uni-due.de>
2023-02-16 00:29:52 +0100

    Implemented show/hide annotation replies

--- webapp/src/app/pages/components/annotations/pdf-annotation/pdf-reply-panel/pdf-reply-panel.component.ts
+++ webapp/src/app/pages/components/annotations/pdf-annotation/pdf-reply-panel/pdf-reply-panel.component.ts
@@ -9,10 +9,26 @@ import { Reply } from 'src/app/models/Reply';
 })
 export class PdfReplyPanelComponent implements OnInit, OnChanges  {
   @Input() annotation: Annotation;
+  repliesCount: number;
+  shownRepliesCount: number = 1;
+  hideRepliesButton: boolean = false;
   constructor() {}
 
   ngOnChanges(changes: SimpleChanges): void {
+    if('annotation' in changes){
+      this.repliesCount = this.annotation.replies.length
+    }
   }
 
   ngOnInit(): void {}
+
+  showAllReplies(){
+    this.shownRepliesCount = this.repliesCount;
+    this.hideRepliesButton = true;
+  }
+
+  hideReplies(){
+    this.shownRepliesCount = 1;
+    this.hideRepliesButton = false;
+  }
 }
