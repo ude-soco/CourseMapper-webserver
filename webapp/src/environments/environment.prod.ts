95a8a379cef63fd84cab49c0b96520897a3d7b0e
Ralf Berger <ralf.berger@uni-due.de>
2023-03-26 19:23:53 +0200

    Append /api to URLs in production

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
-  API_URL: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/api',
+  API_URL: 'http://localhost:8080/api',
 };
 
 export const environment_2 = {
   production: true,
-  apiUrl: 'http://localhost:8080',
-  API_URL: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/api',
+  API_URL: 'http://localhost:8080/api',
 };
