37dc3816cced3f9c150b0ccde803998dd4698e06
Shoeb Joarder <shoebjoarder@outlook.com>
2023-03-27 10:02:15 +0200

    environment value fix
    - fix sign-in & sign-out issues due to incorrect reference to environment values

--- webapp/src/app/config/config.ts
+++ webapp/src/app/config/config.ts
@@ -4,7 +4,7 @@ import { environment, environment_2 } from '../../environments/environment';
 export const USER_KEY = 'coursemapper-user';
 
 export const AUTH_API = `${environment.apiUrl}/api/auth/`;
-export const AUTH_API_2 = `${environment_2.apiUrl}/auth/`;
+export const AUTH_API_2 = `${environment.apiUrl}/auth/`;
 
 export const HTTPOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
