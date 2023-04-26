9878e2f8f7e943c96f9cf91c8842b331459dd1ba
Shoeb Joarder <shoebjoarder@outlook.com>
2023-04-27 01:03:45 +0200

    Update socketio.js

--- webserver/src/socketio.js
+++ webserver/src/socketio.js
@@ -1,18 +1,25 @@
 const socketIO = require("socket.io");
+import dotenv from "dotenv";
+dotenv.config();
+const env = process.env.NODE_ENV || "production";
 
 let io;
 
 module.exports = {
   init: (server) => {
-    io = socketIO(server, {
-      path: "/api/socket.io",
-      cors: {
-        origin: ["http://localhost:4200", process.env.WEBAPP_URL],
-        methods: ["GET", "POST"],
-        allowedHeaders: ["*"],
-        credentials: true,
-      },
-    });
+    env !== "production"
+      ? (io = socketIO(server, {
+          path: "/api/socket.io",
+          cors: {
+            origin: ["http://localhost:4200", process.env.WEBAPP_URL],
+            methods: ["GET", "POST"],
+            allowedHeaders: ["*"],
+            credentials: true,
+          },
+        }))
+      : (io = socketIO(server, {
+          path: "/api/socket.io",
+        }));
 
     return io;
   },
