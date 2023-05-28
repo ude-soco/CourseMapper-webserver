96039fb64c99bad73f8067ccb3053245003fd906
Ralf Berger <ralf.berger@uni-due.de>
2023-05-28 20:28:49 +0200

    Remove mysterious environment_2

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -3,9 +3,3 @@ export const environment = {
   apiUrl: 'DUMMY_BASE_URL',
   API_URL: 'DUMMY_BASE_URL',
 };
-
-export const environment_2 = {
-  production: true,
-  apiUrl: 'DUMMY_BASE_URL',
-  API_URL: 'DUMMY_BASE_URL',
-};
