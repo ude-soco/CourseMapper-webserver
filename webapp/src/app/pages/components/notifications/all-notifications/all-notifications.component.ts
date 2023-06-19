dd271d162d8a28add84eeed7db4e6d1619c5bba0
Ebrahim Huned Karjatwala <62338185+ebrahim78652@users.noreply.github.com>
2023-06-19 12:50:54 +0200

    implement toggling by notification type

--- webapp/src/app/pages/components/notifications/all-notifications/all-notifications.component.ts
+++ webapp/src/app/pages/components/notifications/all-notifications/all-notifications.component.ts
@@ -10,6 +10,11 @@ import {
 import { Router } from '@angular/router';
 import { CourseService } from 'src/app/services/course.service';
 import { HttpClient } from '@angular/common/http';
+import {
+  Notification,
+  NotificationCategory,
+} from 'src/app/models/Notification';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-all-notifications',
@@ -22,6 +27,35 @@ export class AllNotificationsComponent extends BaseNotificationDashboardComponen
   numberOptions = [5, 10, 15, 20];
   selectedNumber = 5;
   value: string = 'Admin notifications';
+  categories = [
+    {
+      label: 'Course Updates',
+      value: NotificationCategory.CourseUpdate,
+    },
+    {
+      label: 'Comments and Mentions',
+      value: NotificationCategory.CommentsAndMentioned,
+    },
+    {
+      label: 'Annotations',
+      value: NotificationCategory.Annotations,
+    },
+  ];
+
+  selectedCategory = [
+    {
+      label: 'Course Updates',
+      value: NotificationCategory.CourseUpdate,
+    },
+    {
+      label: 'Comments and Mentions',
+      value: NotificationCategory.CommentsAndMentioned,
+    },
+    {
+      label: 'Annotations',
+      value: NotificationCategory.Annotations,
+    },
+  ];
 
   //obsevable streams
   courseUpdatesNotifications$ = this.store.select(
@@ -31,6 +65,12 @@ export class AllNotificationsComponent extends BaseNotificationDashboardComponen
     getCommentsAndMentionedNotifications
   );
   annotationsNotifications$ = this.store.select(getAnnotationsNotifications);
+  isCourseUpdatesNotificationsVisible$ = new BehaviorSubject<boolean>(true);
+  isCommentsAndMentionedNotificationsVisible$ = new BehaviorSubject<boolean>(
+    true
+  );
+  isAnnotationsNotificationsVisible$ = new BehaviorSubject<boolean>(true);
+
   constructor(
     protected override store: Store<State>,
     protected override router: Router,
@@ -43,6 +83,36 @@ export class AllNotificationsComponent extends BaseNotificationDashboardComponen
   override ngOnInit(): void {
     this.httpClient.get('assets/data.json').subscribe((data) => {
       console.log(data);
+    });
+  }
+
+  onFiltersModified($event: any) {
+    console.log($event);
+    this.categories.forEach((category) => {
+      const found = $event.value.find(
+        (element: any) => element.value === category.value
+      );
+      if (found) {
+        if (category.value === NotificationCategory.CourseUpdate) {
+          this.isCourseUpdatesNotificationsVisible$.next(true);
+        }
+        if (category.value === NotificationCategory.CommentsAndMentioned) {
+          this.isCommentsAndMentionedNotificationsVisible$.next(true);
+        }
+        if (category.value === NotificationCategory.Annotations) {
+          this.isAnnotationsNotificationsVisible$.next(true);
+        }
+      } else {
+        if (category.value === NotificationCategory.CourseUpdate) {
+          this.isCourseUpdatesNotificationsVisible$.next(false);
+        }
+        if (category.value === NotificationCategory.CommentsAndMentioned) {
+          this.isCommentsAndMentionedNotificationsVisible$.next(false);
+        }
+        if (category.value === NotificationCategory.Annotations) {
+          this.isAnnotationsNotificationsVisible$.next(false);
+        }
+      }
     });
   }
 }
