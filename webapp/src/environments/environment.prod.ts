1e614dfe5d9c3cffb60601f64522ff760a7bde0b
jeanqussa <jean.qussa@gmail.com>
2024-01-06 09:49:30 +0100

    Change DUMMY_BASE_URL to DUMMY_COURSE_MAPPER_URL
    And move /api to webapp/src/environments/environment.prod.ts.
    There is a variable inside of the openai library named DUMMY_BASE_URL.
    The script webapp/nginx/docker-entrypoint.d/90-base-url.sh is replacing
    this variable, thus breaking the frontend build. This fix makes it less
    likely that there is a collision in variable names.

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,18 +1,12 @@
 export const environment = {
   production: true,
-  API_URL: 'DUMMY_BASE_URL',
+  API_URL: 'DUMMY_COURSEMAPPER_BASE_URL/api',
   socketConfig: {
     url: '/',
     options: {},
   },
 };
 
-// TODO: The environment variables are required to be added for production
-// export const environment_Python = {
-//   PYTHON_SERVER: 'DUMMY_PYTHON_SERVER_URL',
-// };
-// export const environment_Neo4j = {
-//   neo4j_url: 'DUMMY_NEO4J_URL',
-//   neo4j_name: 'DUMMY_NEO4J_NAME',
-//   neo4j_pass: 'DUMMY_NEO4J_PASS',
-// };
+export const environment_Python = {
+  PYTHON_SERVER: 'DUMMY_COURSEMAPPER_BASE_URL/kg-api/',
+};
