bb5fb5695035cca1ce9a26e242b8a2935a75f379
navesaurus <anyatmnt@gmail.com>
2024-07-11 05:39:19 +0200

    added new prerequisite functions

--- coursemapper-kg/app/services/course_materials/prerequisite/prerequisite_wrapper.py
+++ coursemapper-kg/app/services/course_materials/prerequisite/prerequisite_wrapper.py
@@ -7,25 +7,38 @@ from prerequisite_relationship import Prerequisite
 class Prerequisite:
     def __init__(self,course_name = "precalc") -> None:
         self.course_name = course_name
+        self.db = DBConnection()
+        self.concepts = pd.DataFrame()
 
-    def find_prerequisite(self):
- 
+    def find_prerequisite_course(self):
         print("get list of courses")
         mongodb = CourseMaterials(course_name=self.course_name)
         learning_materials = mongodb.get_list_of_materials()
 
 
         print("get concepts from neo4j")
-        concepts = pd.DataFrame()
-        db = DBConnection()
-        for _,lm in learning_materials.iterrows():
-            concepts_one = db.extract_concepts(learning_material=lm["name"])
-            concepts_one["createdAt"] = lm["createdAt"]
-            concepts = pd.concat([concepts,concepts_one],ignore_index=True)
-
+       
         
+        for _,lm in learning_materials.iterrows():
+            self.find_prerequisite_one(lm)
+        
+        self.find_prerequsite_all()
+
+    
+    def find_prerequisite_lm(self,lm=None):
+        self.find_prerequisite_one(lm)
+        self.find_prerequsite_all()
+
+    def find_prerequisite_one(self,lm):
+        concepts_one = self.db.extract_concepts(learning_material=lm["name"])
+        concepts_one["createdAt"] = lm["createdAt"]
+        self.concepts = pd.concat([self.concepts,concepts_one],ignore_index=True)
+
+    
+    def find_prerequsite_all(self):
+
         print("clean data")
-        clean_data = DataCleaning(concepts)
+        clean_data = DataCleaning(self.concepts)
         concept_dict = clean_data.get_clean_data()
         related_relationships = clean_data.get_related_relationships()
 
@@ -40,6 +53,6 @@ class Prerequisite:
 
         for _,r in prerequisite_relationships.iterrows():
             try:
-                db.add_prerequisite_connections(r["prerequisite_concept"],r["concept"],r["score_weighted"],r["score_unweighted"])
+                self.db.add_prerequisite_connections(r["prerequisite_concept"],r["concept"],r["score_weighted"],r["score_unweighted"])
             except Exception as e:
                 print(e)
