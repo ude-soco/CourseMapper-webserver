d438ce026a6390c5930279afe8453095a295a1ea
navesaurus <anyatmnt@gmail.com>
2024-08-01 10:28:05 +0200

    Fixed neo4j_connection.py

--- coursemapper-kg/app/services/course_materials/prerequisite/neo4j_connection.py
+++ coursemapper-kg/app/services/course_materials/prerequisite/neo4j_connection.py
@@ -1,10 +1,8 @@
 from neo4j import GraphDatabase
 import pandas as pd
 
-from config import Config
-
 class DBConnection:
-    def __init__(self, uri=Config.NEO4J_URI,username=Config.NEO4J_USER,password=Config.NEO4J_PASSWORD):
+    def __init__(self, uri="bolt://localhost:7687",username="neo4j",password="password",):
         # Define the connection URI, username, and password
         self.uri = uri 
         self.username = username
@@ -29,13 +27,19 @@ class DBConnection:
             with driver.session() as session:
                 result = session.run(self.cypher_query)
                 return result.data()
+
             
+    def extract_main_concepts_query(self, tx,lm):
+        result = tx.run("""MATCH (lm:LearningMaterial {name: $name})-[:LM_CONSISTS_OF]->(concept:Concept {type: 'main_concept'})RETURN concept""",name=self.learning_material)
+        return list(result)
+    
     def extract_main_concepts(self):
         self.learning_material = self.learning_material.replace("'", "\\'")
-        self.cypher_query = """MATCH (lm:LearningMaterial {name: '""" + self.learning_material +"""'})-[:LM_CONSISTS_OF]->(concept:Concept {type: 'main_concept'})RETURN concept"""
         concepts = []
         try:
-            results = self.run_query()
+            with GraphDatabase.driver(self.uri, auth=(self.username, self.password)) as driver:
+                with driver.session() as session:
+                    results = session.execute_read(self.extract_main_concepts_query,self.learning_material)
             for record in results:
                     concept = {
                     "name": record["concept"]["name"],
@@ -48,16 +52,22 @@ class DBConnection:
                     }
                     concepts.append(concept)
             return concepts
-        except:
+        except Exception as e:
+            print(e)
             return None
+    
+    def extract_related_concepts_query(self,tx,name,lm):
+        result = tx.run("""MATCH (lm:LearningMaterial {name: $lm})-[:LM_CONSISTS_OF]->(concept:Concept {name: $name})-[:RELATED_TO]->(related:Concept) RETURN concept,related""",lm=self.learning_material,name=name)
+        return list(result)
+
     def extract_related_concepts(self,name):
         # Define the Cypher query
         self.learning_material = self.learning_material.replace("'", "\\'")
         name = name.replace("'", "\\'")
-        self.cypher_query = """MATCH (lm:LearningMaterial {name: '""" + self.learning_material +"""'})-[:LM_CONSISTS_OF]->(concept:Concept {name: '""" + name +"""'})-[:RELATED_TO]->(related:Concept) RETURN concept,related"""
 
-        
-        results = self.run_query()
+        with GraphDatabase.driver(self.uri, auth=(self.username, self.password)) as driver:
+            with driver.session() as session:
+                results = session.execute_read(self.extract_related_concepts_query,name,self.learning_material)
         for record in results:
             try:
                 concept = {
@@ -73,10 +83,27 @@ class DBConnection:
             except:
                 pass
 
+    def add_prerequisite_connections_query(self,tx,c1,c2,unweighted_weight,weighted_weight):
+        unweighted_weight = str(unweighted_weight)
+        weighted_weight = str(weighted_weight)
+        result = tx.run("""
+            MATCH (concept1:Concept {name: $c1})-[r:PREREQUISITE_TO]->(concept2:Concept {name: $c2})
+            RETURN r
+            """, c1=c1, c2=c2)
+
+        # If no relationship exists, create one
+        if not result.single(): 
+            tx.run("""
+            MERGE (concept1:Concept {name: $c1})
+            MERGE (concept2:Concept {name: $c2})
+            MERGE (concept1)-[:PREREQUISITE_TO {unweighted_weight: $unweighted_weight, weighted_weight: $weighted_weight}]->(concept2)
+            """, c1=c1, c2=c2, unweighted_weight=unweighted_weight, weighted_weight=weighted_weight)
+    
     def add_prerequisite_connections(self,c1,c2,weighted_weight,unweighted_weight):
         c1 = c1.replace("'", "\\'")
         c2 = c2.replace("'", "\\'")
-        self.cypher_query = """MERGE (concept1:Concept {name: '""" + c1 +"""'}) MERGE (concept2:Concept {name: '""" + c2 +"""'}) MERGE (concept1)-[:PREREQUISITE_TO {unweighted_weight: '""" + str(unweighted_weight) +"""',weighted_weight: '""" + str(weighted_weight) +"""'}]->(concept2)"""
-        self.run_query()
+        with GraphDatabase.driver(self.uri, auth=(self.username, self.password)) as driver:
+            with driver.session() as session:
+                session.execute_write(self.add_prerequisite_connections_query,c1,c2,unweighted_weight,weighted_weight)
         
 
