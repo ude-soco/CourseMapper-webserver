7a511e0cb9f2b8ab63d032d5b54613ab269b8156
jeanqussa <jean.qussa@proton.me>
2024-09-01 21:01:41 +0200

    Use URL of hosted instance of DBpedia Spotlight

--- coursemapper-kg/concept-map/src/config.py
+++ coursemapper-kg/concept-map/src/config.py
@@ -19,7 +19,7 @@ class Config:
     NEO4J_PASSWORD = os.getenv('NEO4J_PASSWORD', 'password')
     NEO4J_SAVE_TO_DB = os.getenv('NEO4J_SAVE_TO_DB', 'true').lower() == 'true'
 
-    DBPEDIA_SPOTLIGHT_URL = os.getenv('DBPEDIA_SPOTLIGHT_URL', 'https://api.dbpedia-spotlight.org/en/annotate')
+    DBPEDIA_SPOTLIGHT_URL = os.getenv('DBPEDIA_SPOTLIGHT_URL', 'https://dbpedia-spotlight-en.soco.inko.cloud/rest/annotate')
     DBPEDIA_SPOTLIGHT_CONFIDENCE = float(os.getenv('DBPEDIA_SPOTLIGHT_CONFIDENCE', '0.35'))
     DBPEDIA_SPOTLIGHT_SUPPORT = int(os.getenv('DBPEDIA_SPOTLIGHT_SUPPORT', '5'))
 
