514e8fc4d1655f4dc9af92460ccfc6cd7baf2b62
jeanqussa <jean.qussa@proton.me>
2024-10-05 19:58:51 +0200

    Refine wp-pg-init service #1083

--- coursemapper-kg/wp-pg/bin/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Stream the initialization script to postgres
-echo "${0}: Downloading and executing initialization script..."
-rclone cat "$INIT_PATH" | gunzip | psql
-
-# Move the tmp checksum to the actual checksum
-mv /var/lib/postgresql/meta/init.tmp.sql.gz.sha256 /var/lib/postgresql/meta/init.sql.gz.sha256
